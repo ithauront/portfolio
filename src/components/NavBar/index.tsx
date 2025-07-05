387a2b5619d3d6438573a30c77c4e00be62de3bb
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-07-05 08:34:30 +0200

    feat[components/navbar]: navbar  with sreact scrool

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,65 @@
+import { Link } from "react-scroll";
+
+import Logo from "../../assets/gengarlaxLogo-removebg-preview.png";
+import { Button } from "../Button";
+
+import "./styles.css";
+
+const NAVBAR_HEIGHT = -160;
+
+export function NavBar() {
+  return (
+    <div className="navbar-container">
+      <img src={Logo} alt="Logo" width={50} height={50} />
+      <div className="navbar-links-container">
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={NAVBAR_HEIGHT}
+          activeClass="active"
+          className="navbar-link"
+        >
+          Home
+        </Link>
+        <Link
+          to="aboutMe"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={NAVBAR_HEIGHT}
+          activeClass="active"
+          className="navbar-link"
+        >
+          About me
+        </Link>
+        <Link
+          to="portfolio"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={NAVBAR_HEIGHT}
+          activeClass="active"
+          className="navbar-link"
+        >
+          Portfolio
+        </Link>
+        <Link
+          to="contact"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={NAVBAR_HEIGHT}
+          activeClass="active"
+          className="navbar-link"
+        >
+          Contact me
+        </Link>
+      </div>
+      <div className="navbar-button-container">
+        <Button label="Send e-mail" />
+      </div>
+    </div>
+  );
+}
