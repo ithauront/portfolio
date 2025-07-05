21724a4804230eb98948c7daa74fc5a956639853
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-07-05 08:32:23 +0200

    feat[components/button]: added variant for button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,28 @@
-import './styles.css'
+import "./styles.css";
 
 type Props = {
   label: string;
+  variant?: "primary" | "secondary";
 };
 
-export function Button({label}: Props) {
-    return(
-        <button className='container'>
-            <p>{label}</p>
-        </button>
-    )
+export function Button({ label, variant = "primary" }: Props) {
+  return (
+    <button
+      className={
+        variant === "primary"
+          ? "button-container-primary"
+          : "button-container-secondary"
+      }
+    >
+      <p
+        className={
+          variant === "primary"
+            ? "button-label-primary"
+            : "button-label-secondary"
+        }
+      >
+        {label}
+      </p>
+    </button>
+  );
 }
